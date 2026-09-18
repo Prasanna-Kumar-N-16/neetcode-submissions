472f54147818b4164f2f6770bd8557a94705f840
Prasanna-Kumar-Nagaboyina-16 <84060804+Prasanna-Kumar-N-16@users.noreply.github.com>
2026-09-17 22:29:25 -0500

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.go
@@ -0,0 +1,13 @@
+func rotate(matrix [][]int) {
+	// Reverse the matrix vertically
+	for i, j := 0, len(matrix)-1; i < j; i, j = i+1, j-1 {
+		matrix[i], matrix[j] = matrix[j], matrix[i]
+	}
+
+	// Transpose the matrix
+	for i := 0; i < len(matrix); i++ {
+		for j := i + 1; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
